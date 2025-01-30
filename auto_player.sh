a19f594d4045c516912154e358199378377ef8d3
ryokucha <87602876+pirothat@users.noreply.github.com>
2025-01-30 12:02:45 +0900

    Update auto_player.sh

--- auto_player.sh
+++ auto_player.sh
@@ -5,7 +5,7 @@ MUSIC_DIR=""
 while true
 do
   # find + shuf の出力を "while read -r f" で1行ずつ受け取る
-  find "$MUSIC_DIR" -type f -name '*.flac' 2>/dev/null | shuf | \
+  find "$MUSIC_DIR" -type f -name '*' 2>/dev/null | shuf | \
   while IFS= read -r f
   do
     mpv --no-video --term-status-msg="Playing: ${f}" "$f"
